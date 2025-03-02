a0cd8379c8a94eeb539e1b8e30704747a57ae3f1
RyoSat0 <ryo.sato@mail.concordia.ca>
2025-03-01 22:31:21 -0500

    Added a welcome component

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import ChatSidebar from "../components/ChatSidebar";
+import Welcome from "../components/Welcome";
 import { styles } from "../../styles/styles";
 import { auth0 } from "../../../lib/auth0";
 
@@ -12,12 +13,12 @@ export default async function HomePage() {
     <div style={styles.container}>
       <Navbar SESSION={session} />
 
-      <div style={{ display: "flex" }}>
+      <div style={{ display: "flex", height: "calc(100vh - 21vh)" }}>
         <Suspense fallback={<div>Loading...</div>}>
           <ChatSidebar session={session} />
         </Suspense>
-        <div>
-          <h1>LOGIC TO CREATE USER INFORMATION NEEDS TO BE CREATED</h1>
+        <div style={{ flex: 1, display: "flex" }}>
+          <Welcome />
         </div>
       </div>
 
