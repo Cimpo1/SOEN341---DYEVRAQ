ebfe6ae33edd38cce146104697ce09c0605f3d91
Cimpo1 <125995046+Cimpo1@users.noreply.github.com>
2025-04-08 17:05:18 -0400

    Added more tests

--- tests/testingWEB.spec.ts
+++ tests/testingWEB.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 test('Is the website up?', async ({ page }) => {
-    await page.goto('http://100.98.33.229:3000/');
+    await page.goto('http://localhost:3000/');
     await expect(page.getByRole('link', { name: 'Login' })).toBeVisible();
     await expect(page.getByRole('heading', { name: 'DYEVRAQ', exact: true })).toBeVisible();
 });
 
 test('Is the login Auth0 working?', async ({ page }) => {
-    await page.goto('http://100.98.33.229:3000/');
+    await page.goto('http://localhost:3000/');
     await expect(page.getByRole('link', { name: 'Login' })).toBeVisible();
     await page.getByRole('link', { name: 'Login' }).click();
     await page.getByRole('textbox', { name: 'Email address' }).click();
@@ -89,4 +89,44 @@ test('Can you write a text?', async ({ page }) => {
     await expect(page.locator('body')).toContainText('Test');
     await page.getByRole('link', { name: 'Logout' }).click();
     await expect(page.getByRole('link', { name: 'Login' })).toBeVisible();
+});
+
+
+test('Can you chat with the bot?', async ({ page }) => {
+    await page.goto('http://localhost:3000/');
+    await expect(page.getByRole('link', { name: 'Login' })).toBeVisible();
+    await page.getByRole('link', { name: 'Login' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).fill('d_cimpoi@live.concordia.ca');
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('mF6T5aeebCGk8R4!');
+    await page.getByRole('button', { name: 'Continue', exact: true }).click();
+    await expect(page.getByRole('img', { name: 'Welcome' })).toBeVisible();
+    await expect(page.locator('div').filter({ hasText: /^🤖$/ }).nth(1)).toBeVisible();
+    await page.locator('div').filter({ hasText: /^🤖$/ }).nth(1).click();
+    await page.getByRole('textbox', { name: 'Type a message...' }).click();
+    await page.getByRole('textbox', { name: 'Type a message...' }).fill('Hello');
+    await page.getByRole('textbox', { name: 'Type a message...' }).press('Enter');
+    await expect(page.locator('body')).toContainText('Hello');
+    await page.getByRole('link', { name: 'Logout' }).click();
+    await expect(page.getByRole('link', { name: 'Login' })).toBeVisible();
+});
+
+test('Cannot access home without login', async ({ page }) => {
+    await page.goto('http://localhost:3000/home');
+    await expect(page.getByRole('heading', { name: 'Application error: a client-' })).toBeVisible();
+});
+
+test('Make sure you can sign up', async ({ page }) => {
+    await page.goto('http://localhost:3000/');
+    await expect(page.locator('section').filter({ hasText: 'Join Millions of Users' }).getByRole('link')).toBeVisible();
+    await page.locator('section').filter({ hasText: 'Join Millions of Users' }).getByRole('link').click();
+    await expect(page.getByText('Sign Up to dev-')).toBeVisible();
+});
+
+test('Make sure content loads with css', async ({ page }) => {
+    await page.goto('http://localhost:3000/');
+    await expect(page.getByRole('img', { name: 'LOGO' })).toBeVisible();
+    await expect(page.getByText('© 2025 DYEVRAQ. All rights')).toBeVisible();
+    await expect(page.getByText('Community ServerCreate public')).toBeVisible();
 });
